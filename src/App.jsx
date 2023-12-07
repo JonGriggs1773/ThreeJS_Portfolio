95340604e1eab4f645aae3286784f34986d2227f
Jon Griggs <bigmac_beats1773@outlook.com>
2023-12-06 22:34:39 -0600

    added svgs, and finished about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
-import {Home, About, Projects, Contact} from './pages'
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import { Home, About, Projects, Contact } from './pages'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 //TODO https://www.youtube.com/watch?v=FkowOdMjvYo
 //TODO Finished at 2:13:00
 
-
 const App = () => {
-    return (
-        <main className='bg-slate-300/20'>
-            <Router>
-            <Navbar />
-                <Routes>
-                    <Route path='/' element={<Home />}/>
-                    <Route path='/about' element={<About />}/>
-                    <Route path='/projects' element={<Projects />}/>
-                    <Route path='/contact' element={<Contact />}/>
-                </Routes>
-            </Router>
-        </main>
-    )
+	return (
+		<main className="bg-slate-300/20 h-[100vh]">
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/contact" element={<Contact />} />
+				</Routes>
+			</Router>
+		</main>
+	)
 }
 
 export default App
--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,52 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {arrow} from '../assets/icons'
+import { arrow } from '../assets/icons'
 
-const InfoBox = ({text, link, btnText}) => (
-    <div className='info-box'>
-        <p className='font-medium sm:text-xl text-center'>{text}</p>
-        <Link to={link} className='neo-brutalism-white neo-btn'>
-            {btnText}
-            <img src={arrow} className='w-4 h-4 object-contain' />
-        </Link>
-    </div>
+const InfoBox = ({ text, link, btnText }) => (
+	<div className="info-box">
+		<p className="font-medium sm:text-xl text-center">{text}</p>
+		<Link to={link} className="neo-brutalism-white neo-btn">
+			{btnText}
+			<img src={arrow} className="w-4 h-4 object-contain" />
+		</Link>
+	</div>
 )
 
 const renderContent = {
-    1: (
-        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-            Hi, I am <span className='font-semibold'>Jonathen</span>
-            <br/>
-            A Full-Stack Software engineer from <br/> Tennesse, United States
-        </h1>
-    ),
-    2: (
-        <InfoBox
-        text="Currently, I work as a Software Engineer at Colorado Technical University"
-        link='/about'
-        btnText='Learn More'
-        />
-    ),
-    3: (
-        <InfoBox 
-        text="Feel free to check out projects I have led and assisted with throughout the years!"
-        link='/projects'
-        btnText='Visit My Portfolio'
-        />
-    ),
-    4: (
-        <InfoBox 
-        text="Want to collaborate on a project or just simply looking for a dev?"
-        link='/contact'
-        btnText='Shoot Me A Message!'
-        />
-    )
+	1: (
+		<h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+			Hi, I am <span className="font-semibold">Jonathen</span>
+			<br />
+			A Full-Stack Software engineer from <br /> Tennesse, United States
+		</h1>
+	),
+	2: (
+		<InfoBox
+			text="Currently, I work as a Software Engineer at Colorado Technical University"
+			link="/about"
+			btnText="Learn More"
+		/>
+	),
+	3: (
+		<InfoBox
+			text="Feel free to check out projects I have led and assisted with throughout the years!"
+			link="/projects"
+			btnText="Visit My Portfolio"
+		/>
+	),
+	4: (
+		<InfoBox
+			text="Want to collaborate on a project or just simply looking for a dev?"
+			link="/contact"
+			btnText="Shoot Me A Message!"
+		/>
+	)
 }
 
-
-const HomeInfo = ({currentStage}) => {
-
-    return renderContent[currentStage] || null
+const HomeInfo = ({ currentStage }) => {
+	return renderContent[currentStage] || null
 }
 
 export default HomeInfo
